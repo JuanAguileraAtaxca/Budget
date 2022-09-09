1bae3b96bd62f68b210793c164396626169bff0e
Juan Jose Aguilera <aguileraataxca@gmail.com>
2022-09-08 21:50:25 -0500

    Third commit

--- src/components/BudgetMain/BudgetMain.jsx
+++ src/components/BudgetMain/BudgetMain.jsx
@@ -5,7 +5,8 @@ import {useState} from 'react';
 
 const BudgetMain = ({budget, setBudget}) => {
 
-    const [modal, setModal] = useState(false); 
+    const [modal, setModal] = useState(false);
+    const [available, setAvailable] = useState(budget); 
     const [items, setItems] = useState([]); 
 
     const format = (value) => {
@@ -17,15 +18,15 @@ const BudgetMain = ({budget, setBudget}) => {
 
     return(
         <>
-            {modal && <Modal items={items} setItems={setItems} modal={modal} setModal={setModal}/> }
+            {modal && <Modal items={items} setItems={setItems} modal={modal} setModal={setModal} setAvailable={setAvailable} available={available}/> }
             <div className={style.BudgetMainContainer + " m-c mt-100"}>
                 <div className='center'>
                     <div className={style.graphTest}></div>
                 </div>
                 <div> 
                     <p className={style.BudgetMainText}> Presupuesto: <span className={style.BudgetMainTextValue}> {format(budget)} </span></p>
-                    <p className={style.BudgetMainText}> Saldo disponible: <span className={style.BudgetMainTextValue}> {format(0)} </span></p>
-                    <p className={style.BudgetMainText}> Gastos: <span className={style.BudgetMainTextValue}> {format(0)}</span></p>
+                    <p className={style.BudgetMainText}> Saldo disponible: <span className={style.BudgetMainTextValue}> {format(available)} </span></p>
+                    <p className={style.BudgetMainText}> Gastos: <span className={style.BudgetMainTextValue}> {format(budget - available)}</span></p>
                     <button className={style.BudgetMainButton} onClick={() => setBudget(0)}>
                         Reset budget
                     </button>
@@ -34,7 +35,7 @@ const BudgetMain = ({budget, setBudget}) => {
             </div>
             <div>
                 <ul>
-                    {items.map(item => <li> {item.name} </li>)}
+                    {items.map(item => <li key={item.id}> {item.name}  </li>)}
                 </ul>
                 
             </div>
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,65 @@
 import style from './Modal.module.css'; 
 import { FaTimesCircle } from "react-icons/fa";
-import {useState} from 'react'; 
+import {useState} from 'react';
+import MessageError from '../MessageError/MessageError'; 
 
 
-const Modal = ({items, setItems, modal, setModal}) => {
+const Modal = ({items, setItems, modal, setModal, setAvailable, available}) => {
 
     const [item, setItem] = useState(''); 
     const [price, setPrice] = useState(0);
     const [category, setCategory] = useState('');  
+    const [validation, setValidation] = useState(false); 
 
     const hadleModal = (e) =>{
         e.preventDefault(); 
 
-        if([item, price, category].includes('')){
-            console.log("there's field empty"); 
+        if([item, price, category].includes('') || price <= 0){
+            setValidation(true);
             return; 
         }
 
-        const newItem = {
-            name: item,
-            priceItem: Number(price), 
-            categoryItem: category
+        if(available >= price){
+            const newItem = {
+                id: generateId(),
+                name: item,
+                priceItem: Number(price), 
+                categoryItem: category,
+                date: generateDate()
+            }
+    
+            setAvailable(available - price); 
+    
+            setItems([...items, newItem]);  
+            setValidation(false); 
+            console.log(newItem); 
+    
+            setItem('');
+            setPrice(0); 
+            setCategory(""); 
         }
 
-        setItems([...items, newItem]);  
+        
 
-        setItem('');
-        setPrice(0); 
-        setCategory(""); 
+    }
 
+    const generateDate = () => {
+        const date = new Date(); 
+        const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let formatDate = "/" + month[date.getMonth() + 1] + "/" + date.getFullYear(); 
+
+        if(date.getDate() < 10){
+            return "0" + date.getDate() + formatDate;
+        }
+
+        return date.getDate() + formatDate; 
+    }
+
+    const generateId = () =>{
+        const firstPart = Date.now().toString(); 
+        const secondPart = Math.random().toString(); 
+
+        return firstPart.substr(5, firstPart.length) + secondPart.substr(5, 7); 
     }
 
     return(
@@ -36,8 +67,11 @@ const Modal = ({items, setItems, modal, setModal}) => {
             <button onClick={() => setModal(!modal)} className={style.ModalExit + ' center'}>
                 <FaTimesCircle />
             </button>
+
             <form className={style.ModalForm} onSubmit={hadleModal}>
                 <h2 className={style.ModalTitle}> New Item </h2>
+                {validation && <MessageError message={price < 0 ? "Price negative is invalid" : "Empty fields!"}/>}
+
                 <div>
                     <label> Item: </label>
                     <input value={item} onChange={(e) => setItem(e.target.value)} type="text" placeholder='Name item'/>
